d2b6fb01abd04cd6b9cd24ea6ddef31b2b2ab5c0
dudemelo <1172271+dudemelo@users.noreply.github.com>
2025-07-27 18:06:08 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dudemelo/facil
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type config struct {
+	buildDir     string
+	pagesDir     string
+	templatesDir string
+}
+
+func main() {
+	c := config{
+		buildDir:     "example/build/",
+		pagesDir:     "example/pages/",
+		templatesDir: "example/templates/",
+	}
+	checkError(os.RemoveAll(c.buildDir))
+	checkError(os.Mkdir(c.buildDir, 0755))
+
+	dir, err := os.ReadDir(c.pagesDir)
+	checkError(err)
+	for _, file := range dir {
+		fname := file.Name()
+		pname := fname[0 : len(fname)-5]
+		tpl := template.Must(template.ParseFiles(c.pagesDir + file.Name()))
+		tpl = template.Must(template.Must(tpl.Clone()).ParseGlob(c.templatesDir + "*.tmpl"))
+		os.Mkdir(c.buildDir+pname, 0775)
+		index, err := os.Create(c.buildDir + pname + "/index.html")
+		checkError(err)
+		tpl.Execute(index, nil)
+	}
+}
+
+func checkError(e error) {
+	if e != nil {
+		panic(e.Error())
+	}
+}
